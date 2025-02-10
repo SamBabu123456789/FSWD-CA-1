cf4f316d36d63913e97e197cc9048149927115b4
SamBabu123456789 <sambabupolimetla@gmail.com>
2025-02-10 14:24:42 +0530

    done

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const port = 4600
+const app = express()
+
+app.get('/',(req,res)=>{
+    res.send("Hello World")
+})
+
+app.listen(port,()=>{
+    console.log(`server is connected http://localhost${port}`)
+})
+
+app.post('/',(req,res)=>{
+    const { Username , Email , Password , DOB}=req.body
+    if(!Username)
+        return res.status(400).json({message:`Username cannot be empty`})
+    if(!Email)
+        return res.status(400).json({message:`Email cannot be empty`})
+    if(Password.length<8 || Password.length>16)
+        return res.status(400).json({message:`Password length should be greater than 8 or less than or equal to 16`})
+    return res.status(200).json({message:`All the details were given`})
+})
